029e6bdbc5c29eefe688e73784bcb05444ce9fdd
Firose Hussain <github@firumon.com>
2022-12-12 11:47:41 +0530

    code modified

--- sw.js
+++ sw.js
@@ -1,5 +1,17 @@
 self.addEventListener('push',(e) => {
-    e.waitUntil(
-        self.registration.sendNotification('SW Notification',{})
-    )
+    console.info('PUSH EVENT ON SW');
+    console.log(e.data)
+    // e.waitUntil(
+    //     self.registration.sendNotification('SW Notification',{})
+    // )
 })
+
+var I = 0;
+setInterval(() => {
+    console.log(I += Math.random())
+    if(I >= 5) {
+        console.info('Console will be cleared soon');
+        setTimeout(console.clear,2000 + Math.random()*1000);
+        I = 0;
+    }
+},5000)
