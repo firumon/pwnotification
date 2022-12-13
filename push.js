a67ceba2278bb3002f3d5742df58980d512955ab
Firose Hussain <github@firumon.com>
2022-12-13 11:45:51 +0530

    code modified

--- push.js
+++ push.js
@@ -6,11 +6,19 @@ let vapidKeys = {"publicKey":"BPOCgjh0gFISZBTBDIANtJB3aNCObYwwRmJQQJu0EWIcjlZGzD
 
 push.setVapidDetails('mailto:me@firumon.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/c0izaOJt1F8:APA91bF2kJG2oJb8mgLMUwVncG8SZvWoF6kmKL6sGM5KJ3cPg0Y91uTApABmRhfsmB3Mu8Va4EnpbGncaMrd7RhN_5D1wIOkpq3mkaWTilb4UWqdiFMgws1tQJt8imLl5IM1vyZ97VOe","expirationTime":null,"keys":{"p256dh":"BE4khbve1ZaIdCoc47DQP1fLY7fO4attMc_mQb8Rtzdl93A0YHKVwixWaLfxCvvr6mg_M4riCWU9gCcDvZnH7Dk","auth":"SEaMP34Du7_ZZAqEgU_97A"}}
-// let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/fQitPgRjaYY:APA91bEwen5_HQjUrj8OUQi5RAR-IACueE9idSdQihDTfrYBqnLBdlmjzZe4S--ZYTFR6QiTdxiwYJS6f-JwqrkCyaiahk4jxDmhhW0oAe0wWDFwZaME4GY1e-abInQ1D5MkYGxvJ9Yv","expirationTime":null,"keys":{"p256dh":"BILPGHsw3XYW4YpsTM16qYiUm-9b4zgLGouwypLPVQ0fULVswBbi7_v6_O4D02CHoFWeMrjg_tq8ChUbrln9Pa0","auth":"f9xRVRpsegUgux12Ygijow"}}
+// let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/c1-ELxx1r_I:APA91bF-QAvibzdmT_7DRru9gRidAyz-V61dW4CUj5aUkLQdLGG7w7XKBAXav-pO881hiImH3pm9c8q1YvY3nSKgCYV4rSn0cgbv1W4ZqqVkFP5GXmEOqoE_sufa5r_fPUHGW8mhbuC0","expirationTime":null,"keys":{"p256dh":"BFRIlM9uE7bpCZZLM0SPlJ1Na6Ntzwn3rKp7-Ykoiwbvi9KeIcpNbEsPvg_-xVwpGhnOBS2ckLeSknlZjcw1wzY","auth":"s-v_ShNz416WofJiD3E94g"}}
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/c7ePClmE9vM:APA91bEvRnmP9eT4p4CAU0pkovY8HOTRlw5MCmG4IfBYD4JrYt3sq7Q5GoIcdgfu7Sy26fTufXJsN59yMKxSnRV7fdFqNpcT-ZruosHDLOLxu-MeIGH9dg5Npl-avXW-EEf062jodHPa","expirationTime":null,"keys":{"p256dh":"BG3f4I0bpYwKwa5xnOZhpTmJKx8vEMd98cDOF-GAMdyPH22o5CU1Nd5fxmPLY3mNg729WcqFgAqG6FyFqO9-a6I","auth":"S8DWN1MaxcxEa6iGtqW7ow"}}
 
 push.sendNotification(sub, JSON.stringify({
-    "title":"I AM NOTIFICATION",
-    "body":"This is notification Body.."
+    "title": "OKEY",
+    "body": "BOOOOOOOODY",
+    "actions": [{
+        "action": "https://www.npmjs.com/package/web-push",
+        "title": "MA ACTION ONE",
+        "icon": "https://cdn-icons-png.flaticon.com/512/3658/3658773.png"
+    }],
+    "icon": "https://play-lh.googleusercontent.com/yPtnkXQAn6yEahOurxuYZL576FDXWn3CqewVcEWJsXlega_nSiavBvmaXwfTGktGlQ",
+    "image": "https://i.pinimg.com/originals/3a/69/ae/3a69ae3942d4a9da6c3cbc93b1c8f051.jpg",
+    "vibrate": [300,300,300]
 }))
 
