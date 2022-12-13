7871a6200b5ee33dc10ddb36982949643e02b432
Firose Hussain <github@firumon.com>
2022-12-13 13:50:16 +0530

    code modified

--- sw.js
+++ sw.js
@@ -4,13 +4,3 @@ self.addEventListener('push',(e) => {
         self.registration.showNotification(jData.title,jData)
     )
 })
-
-var I = 0;
-setInterval(() => {
-    console.log(I += Math.random())
-    if(I >= 5) {
-        console.info('Console will be cleared soon');
-        setTimeout(console.clear,2000 + Math.random()*1000);
-        I = 0;
-    }
-},15000)
